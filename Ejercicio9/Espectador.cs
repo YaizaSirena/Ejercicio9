3cf1ae8afe6882a9e6e218a5470782e152a1a724
YaizaSirena <yaizamartos@hotmail.com>
2021-03-22 21:42:36 +0100

    Empezando

---
+++ Ejercicio9/Espectador.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio9
+{
+    class Espectador
+    {
+        string nombre;
+        int edad;
+        double dinero;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public double Dinero { get => dinero; set => dinero = value; }
+        public string Nombre1 { get => nombre; set => nombre = value; }
+
+
+        public Espectador (string nombrePa, int edadPa, double dineroPa)
+        {
+            Nombre = nombrePa;
+            edad = edadPa;
+            Dinero = dineroPa;
+        }
+    }
+}
